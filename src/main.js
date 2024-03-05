9acafd5802256d3e0402c1ee1cf8d40cd734dd99
improve11 <142506318+improve11@users.noreply.github.com>
2024-03-06 04:49:23 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router/index.js';
 
+createApp(App).use(router).use(store).mount('#app')
