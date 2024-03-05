58ed3824a37c5f538c960925328304ae51ebf81b
improve11 <142506318+improve11@users.noreply.github.com>
2024-03-06 04:52:59 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import TaskList from '../components/TaskList.vue';
+import TaskForm from '../components/TaskForm.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'TaskList',
+    component: TaskList
+  },
+  {
+    path: '/new',
+    name: 'TaskForm',
+    component: TaskForm
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
