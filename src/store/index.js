2271028c566a18926e0d9bab7a340b254d04a0ec
improve11 <142506318+improve11@users.noreply.github.com>
2024-03-06 04:53:37 +0800

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,29 @@
+import { createStore } from 'vuex';
 
+export default createStore({
+  state: {
+    tasks: [],
+  },
+  mutations: {
+    addTask(state, task) {
+      if (!state.tasks.find(t => t.title === task.title && t.description === task.description)) {
+        state.tasks.push(task);
+      }
+    },
+    deleteTask(state, taskId) {
+      state.tasks = state.tasks.filter(task => task.id !== taskId);
+    },
+    editTask(state, updatedTask) {
+      const index = state.tasks.findIndex(task => task.id === updatedTask.id);
+      if (index !== -1) {
+        state.tasks[index] = updatedTask;
+      }
+    }
+  },
+  actions: {
+    
+  },
+  getters: {
+    allTasks: state => state.tasks,
+  },
+});
